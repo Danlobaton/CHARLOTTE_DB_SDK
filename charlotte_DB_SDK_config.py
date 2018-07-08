263f3ac0397cb26d8d8b64bdd12e2a768061023c
Daniel Lobaton <dlobaton@ufl.edu>
2018-07-08 16:38:50 -0400

    rm config file

--- charlotte_DB_SDK_config.py
+++
@@ -1,5 +0,0 @@
-# need to setup IP address and token with support staff
-###########################################################
-IP_ADDRESS_DB = '74.80.245.208:5000' #'XXX PLEASE SETUP'
-DATABASE_TOKEN = 'augi1234qwer' #'XXX PLEASE SETUP'
-###########################################################
